a263fc785fe34fef56f8744cdfbfd1d6a9dd91a6
chocomilito268 <mejia.angel.2024@cbtis-246.edu.mx>
2025-04-04 16:14:00 -0600

    Add files via upload

---
+++ Test_Angel.sql
@@ -0,0 +1,20 @@
+Create table materia (
+	id INT,
+	nombre VARCHAR(30),
+	alumno VARCHAR(30),
+	calificacion INT 
+);
+insert INTO materia (id, nombre, alumno, calificacion) VALUES
+(1,'matematicas','Angel',5),
+(2,'ciencias sociales','Angel',9),
+(3,'ingles','Angel',8),
+(4,'lengua y comunicacion','Angel',9),
+(5,'con energia','Angel',9),
+(6,'CSdSI','Angel',5),
+(7,'ISdSI','Angel',8),
+(8,'cultura digital','Angel',9),
+(9,'DSdSI','Angel',10),
+(10,'Soc Emo','Angel',9);
+
+
+		  
